389ba4d961703ab73168d2bd85a1d85eccd2f029
e9ashinde <167064466+e9ashinde@users.noreply.github.com>
2024-04-19 16:47:17 +0530

    Add files via upload

---
+++ task_2.sh
@@ -0,0 +1,11 @@
+fibonacci() {
+    a=0
+    b=1
+    for ((i=0; i<$1; i++))
+    do
+        echo -n "$a "
+        fn=$((a + b))
+        a=$b
+        b=$fn
+    done
+}
---
+++ task_3.sh
@@ -0,0 +1,10 @@
+$fact = 1
+while ($num -gt 1) {
+      fact = fact * num
+      $fact *= $num
+
+       num = num - 1
+       $num--
+}
+
+
